4359f2e776d9938b4d940239028d7005d51af1fb
Brett Abramson <brett.m.abramson@gmail.com>
2023-06-19 11:35:21 -0700

    reorganized

--- Summation.js
+++
@@ -1,9 +0,0 @@
-var summation = function (num) {
-    let total = 1
-    for(let i = 0; i >= num; i++) {
-      total = total + i
-    }
-    return total
-  }
-
-  console.log(summation(5)) 
--- string_ends_with.js
+++
@@ -1,9 +0,0 @@
-function solution(str, ending){
-    return (ending.length===0) ? true : (()=> {
-        const endOfString = str.substr(-(ending.length))
-        return endOfString===ending
-    })();
-  }
-
-  console.log(solution("abc",""))
-  
