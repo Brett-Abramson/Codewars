9a144910fa9a2c332a212f1df620b5f961c32a86
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-14 11:31:40 -0700

    completed list_filtering

--- kyu 7/list_filtering.js
+++ kyu 7/list_filtering.js
@@ -10,10 +10,12 @@ const filterList = (l) => {
 
 const array = [1,2,'aasf','1','123',123]
 
-console.log(filterList(array))
+// console.log(filterList(array))
 
 
 // Best Practice
 function filter_list(l) {
     return l.filter(function(v) {return typeof v == 'number'})
   }
+
+console.log(filter_list(array))
