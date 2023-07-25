00a2d8826c84e50aa89f8104e6ef7047fc0b766c
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-25 15:57:30 -0700

    completed century_from_year.js

---
+++ kyu 8/century_from_year.js
@@ -0,0 +1,10 @@
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+const century = (year) => Math.ceil(year/100)
+
+// console.log(century(1705))
+
+console.log(Math.ceil(101/100))
