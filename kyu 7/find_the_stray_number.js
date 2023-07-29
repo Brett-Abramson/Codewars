7f29fea5ac961f058d34e2a3439e83c8a47a053f
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-29 13:06:23 -0700

    completed find_the_stray_number.js

---
+++ kyu 7/find_the_stray_number.js
@@ -0,0 +1,18 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+const array = [1, 1, 2]
+
+const stray = (num) => {
+    num.sort()
+        return num[1] !== num[0] 
+        ? num[0]
+        : num[num.length - 1] 
+}
+
+console.log(stray(array))
+
+
