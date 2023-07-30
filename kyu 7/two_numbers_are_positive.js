b785f773f29e2ba586769d8a9e7130c7d37b0034
Brett Abramson <brett.m.abramson@gmail.com>
2023-07-30 11:19:03 -0700

    refactored two_numbers_are_positive.js

--- kyu 7/two_numbers_are_positive.js
+++ kyu 7/two_numbers_are_positive.js
@@ -1,18 +1,23 @@
 // Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
 
-const twoArePositive = ( a, b, c) => {
-  if(isPositive(a) && isPositive(b) && !isPositive(c)) {
-    return true
-  } else if(isPositive(b) && isPositive(c) && !isPositive(a)) {
-    return true
-  } else if(isPositive(c) && isPositive(a) && !isPositive(b)) {
-    return true
-  } else return false
-}
+const twoArePositive = (a, b, c) => {
+  if (isPositive(a) && isPositive(b) && !isPositive(c)) {
+    return true;
+  } else if (isPositive(b) && isPositive(c) && !isPositive(a)) {
+    return true;
+  } else if (isPositive(c) && isPositive(a) && !isPositive(b)) {
+    return true;
+  } else return false;
+};
 
 const isPositive = (num) => {
-    if(num > 0) return true
-}
+  if (num > 0) return true;
+};
 
+console.log(twoArePositive(2, 4, -3));
 
-console.log(twoArePositive(2, 4, -3))
+const twoArePositiveRefactored = (a, b, c) => {
+  const numbers = [a, b, c];
+  const positiveCount = numbers.filter((num) => num > 0).length;
+  return positiveCount === 2;
+};
