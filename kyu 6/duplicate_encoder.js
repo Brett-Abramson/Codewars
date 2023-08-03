275e77b2e7559e943cf2ed9038cd06b8dc8bd48e
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-03 13:18:04 -0700

    completed duplicate_encoder.js

--- kyu 6/duplicate_encoder.js
+++ kyu 6/duplicate_encoder.js
@@ -1,9 +1,20 @@
 // The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
-const duplicateEncoder = (word) => {
-    return word.toLowerCase().split("").filter((val, i)=>{
-        return console.log()
-    })
+const duplicateEncode = (word) => {
+    const ltrAmount = {};
+    const string = word.toLowerCase()
+    
+    for (const ltr of string) {
+        ltrAmount[ltr] ? ltrAmount[ltr]++ : ltrAmount[ltr] = 1
+    }
+    let encoded = []
+    // for each ltr in word check to see its value in ltrAmount is over 1
+    for (const ltr of string) {
+        (ltrAmount[ltr] > 1) 
+        ? encoded.push(")")
+        : encoded.push("(")
+    }
+    return encoded.join("")
 };
 
-console.log(duplicateEncoder("recede"));
+console.log(duplicateEncoder("Recede"));
