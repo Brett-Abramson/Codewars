186d55bb0b1ea2a199b3ceaaf4331bb8a0c7d610
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-17 12:55:49 -0700

    added more efficient solution to sum_array.js

--- kyu 8/sum_Array.js
+++ kyu 8/sum_Array.js
@@ -20,3 +20,11 @@ const sumArray = (array) => {
 
 
 console.log(sumArray(array))
+
+// Best Practice ( O(n) )
+
+function sumArray(array) {
+  return Array.isArray(array) && array.length > 1
+    ? array.reduce((s, n) => s + n, 0) - Math.min(...array) - Math.max(...array)
+    : 0
+}
