227efceefbdc0b43295464671b9d32057e69da9b
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-31 16:55:04 -0500

    completed return_substring_instance_count.js

---
+++ kyu 7/return_substring_instance_count.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it returns the number of times the search_text is found within the full_text.
+
+// Usage example:
+
+// solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+// solution('aaabbbcccc', 'bbb') # should return 1
+// Overlap is not permitted : "aaa" contains 1 instance of "aa", not 2.
+
+const solution = (fullText, searchText) => {
+  let regex = new RegExp(searchText, "g");
+  return regex.test(fullText) ? fullText.match(regex).length : 0;
+};
+
+console.log(solution("aa_bb_cc_dd_bb_e", "bb"));
