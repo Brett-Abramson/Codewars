64d4fc61af875426f0e11c752b66d1b2f4937ffb
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-11 19:04:06 -0400

    added one line refactor function

--- kyu 7/odd_or_even.js
+++ kyu 7/odd_or_even.js
@@ -4,11 +4,15 @@
 
 // If the input array is empty consider it as: [0] (array with a zero).
 
-const input = [0, 5, 3, 2];
+const input = [0, 5, 4, 2];
 
 const oddOrEven = (array) => {
   const sum = array.reduce((sum, num) => (sum += num), 0);
   return sum % 2 === 0 ? "even" : "odd";
 };
 
-console.log(oddOrEven(input));
+const oddOrEven1 = (array) => array.reduce((sum, num) => sum + num, 0) % 2 ? "odd" : "even"
+
+console.log(oddOrEven1(input));
+
+
