0e05443a66282e9f694711cfd8b6fd1c460e0470
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-16 14:50:51 -0500

    completed alternate_capitalization.js

---
+++ kyu 7/alternate_capitalization.js
@@ -0,0 +1,23 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+const input = "abcdef";
+
+const capitalize = (string) => {
+  let evens = "";
+  let odds = "";
+  for (let i = 0; i < string.length; i += 2) {
+    evens += string.charAt(i).toUpperCase() + string.charAt( i + 1)
+    odds += string.charAt(i) + string.charAt(i + 1).toUpperCase()
+  }
+  return [evens, odds]
+};
+
+
+
+console.log(capitalize(input));
