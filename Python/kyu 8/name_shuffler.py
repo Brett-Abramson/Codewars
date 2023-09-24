12f2f940eef76386730a9288bfb8163a0a69be3c
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-24 11:10:17 -0500

    find_the_first_non-consecutive_numers.py completed

---
+++ Python/kyu 8/name_shuffler.py
@@ -0,0 +1,12 @@
+# Write a function that returns a string in which firstname is swapped with last name.
+
+# Example(Input --> Output)
+
+# "john McClane" --> "McClane john"
+
+def name_shuffler(str_):
+  list = str_.split(" ")
+  reverseList = str(list[1]) + " " + str(list[0])
+  return reverseList
+
+print(name_shuffler("Mo Salah"))
