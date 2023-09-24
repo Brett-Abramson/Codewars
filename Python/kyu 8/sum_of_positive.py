cf23966d42d7d6fcf5632f9b8bae4de6dc792e4d
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-24 11:29:13 -0500

    sum_of_positive.py refactored

--- Python/kyu 8/sum_of_positive.py
+++ Python/kyu 8/sum_of_positive.py
@@ -16,3 +16,14 @@ def positive_sum(arr):
 
 
 print(positive_sum(input))
+
+# not considered best practice but an example of pyhton list comprehension
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+# x for x in arr : This is called a generator expression. It's a concise way to create an iterator that yeilds elements from the "arr". It goes through each element in "arr" one by one and temporaily assigns it to "x"
+
+# This generator expression is used inside the sum function in your positive_sum function. It doesn't store multiple numbers in memory; it generates the numbers one by one as needed. It essentially encapsulates a loop over the elements of arr. When you call sum on it, it goes through the elements of arr one by one and calculates their sum without creating an intermediate list to store them.
+
+# In summary, generators are memory-efficient because they generate values on-the-fly as you iterate over them, and they don't store the entire sequence of values in memory at once. Instead, they remember their state and can continue from where they left off.
