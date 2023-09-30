33ff60795504321e910ea71268ee6be92b4c0cb6
Brett Abramson <brett.m.abramson@gmail.com>
2023-09-30 13:07:42 -0500

    reverse_list_order.py completed

---
+++ Python/kyu 8/reverse_list_order.py
@@ -0,0 +1,10 @@
+# In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+input = [2, 3, 4, 1]
+
+
+def reverse_list(l):
+    return l[::-1]
+
+
+print(reverse_list(input))
