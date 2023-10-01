056f6be202fb3bd219804711b149ae348f703d0a
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-01 10:39:39 -0500

    factorial.py recursive solution added

--- Python/kyu 7/factorial.py
+++ Python/kyu 7/factorial.py
@@ -14,3 +14,10 @@ def factorial(n):
 
 
 print(factorial(5))
+
+# RECURSIVE SOLUTION
+
+def factorial(n):
+    if n < 0 or n > 12:
+        raise ValueError
+    return 1 if n <= 1 else n*factorial(n-1)
