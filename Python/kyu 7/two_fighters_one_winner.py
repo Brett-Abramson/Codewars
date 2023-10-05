d11f31df8079217a4492262b2210445f050dc0c3
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-05 11:36:00 -0500

    two_fighters_one_winner.py refactored

--- Python/kyu 7/two_fighters_one_winner.py
+++ Python/kyu 7/two_fighters_one_winner.py
@@ -13,11 +13,10 @@ class Fighter(object):
         self.name = name
         self.health = health
         self.damage_per_attack = damage_per_attack
-        
-    def __str__(self): return "Fighter({}, {}, {})".format(self.name, self.health, self.damage_per_attack)
-    __repr__=__str__
 
-
+    def __str__(self): return "Fighter({}, {}, {})".format(
+        self.name, self.health, self.damage_per_attack)
+    __repr__ = __str__
 
 
 def declare_winner(fighter1, fighter2, first_attacker):
@@ -38,6 +37,15 @@ def declare_winner(fighter1, fighter2, first_attacker):
             if fighter2.health <= 0:
                 return fighter1.name
 
-        
 
 print(declare_winner(Fighter("Mike", 50, 6), Fighter("Evander", 55, 5), "Evander"))
+# REFACTORED
+
+
+def declare_winner(fighter1, fighter2, first_attacker):
+    cur, opp = (fighter1, fighter2) if first_attacker == fighter1.name else (
+        fighter2, fighter1)
+    while cur.health > 0:
+        opp.health -= cur.damage_per_attack
+        cur, opp = opp, cur
+    return opp.name
