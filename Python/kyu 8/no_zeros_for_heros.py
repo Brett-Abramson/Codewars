dd8ca99668c1eb3d0ca0bb93af24aae1e1b466b5
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-10 17:58:11 -0700

    no_zeros_for_heros.py

---
+++ Python/kyu 8/no_zeros_for_heros.py
@@ -0,0 +1,24 @@
+# Numbers ending with zeros are boring.
+
+# They might be fun in your world, but not here.
+
+# Get rid of them. Only the ending ones.
+
+# 1450 -> 145
+# 960000 -> 96
+# 1050 -> 105
+# -1050 -> -105
+# Zero alone is fine, don't worry about it. Poor guy anyway
+
+
+def no_boring_zeros(n):
+    while(n % 10 == 0 and n != 0):
+        n = n / 10
+
+    return int(n)
+        
+
+
+print(no_boring_zeros(0))
+
+
