3f10aa904550e597e0403e31c8a22c3a3ab96f56
Brett Abramson <brett.m.abramson@gmail.com>
2024-02-08 09:56:46 -0800

    bumps_in_the_road.js refactoreD

--- Javascript/kyu 7/bumps_in_the_road.js
+++ Javascript/kyu 7/bumps_in_the_road.js
@@ -2,12 +2,19 @@
 
 // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
-const bump = (x) => { 
-  let bumps = 0
-  for (const letter of x) {
-    if(letter === "n") bumps++
-  }
-  return bumps > 15 ? "Car Dead" : "Woohoo!"
-}
+// const bump = (x) => { 
+//   let bumps = 0
+//   for (const letter of x) {
+//     if(letter === "n") bumps++
+//   }
+//   return bumps > 15 ? "Car Dead" : "Woohoo!"
+// }
 
-console.log(bump("nnnnnnnnnnnnnnnnn_n___n_"));
+
+
+// REFACTORED
+
+const bump = (x) => x.split("n").length>15?"Car Dead":"Woohoo!"
+
+
+ console.log(bump("nnnnnnnnnnnnnnnnn_n___n_"));
