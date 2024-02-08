cfadb592f35bb899a6a802df60b8af4e449db7e6
Brett Abramson <brett.m.abramson@gmail.com>
2024-02-08 09:54:22 -0800

    bumps_in_the_road.js

---
+++ Javascript/kyu 7/bumps_in_the_road.js
@@ -0,0 +1,13 @@
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+const bump = (x) => { 
+  let bumps = 0
+  for (const letter of x) {
+    if(letter === "n") bumps++
+  }
+  return bumps > 15 ? "Car Dead" : "Woohoo!"
+}
+
+console.log(bump("nnnnnnnnnnnnnnnnn_n___n_"));
