f287f4f2eda33ced975e821e28d10dbf53b2c807
Brett Abramson <brett.m.abramson@gmail.com>
2024-02-10 12:10:18 -0800

    create_phone_number.js refactored

--- Javascript/kyu 6/create_phone_number.js
+++ Javascript/kyu 6/create_phone_number.js
@@ -11,3 +11,13 @@ const createPhoneNumber = (numbers) => `(${numbers[0]}${numbers[1]}${numbers[2]}
  
 
  console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+ const createPhoneNumberRefactored = (numbers) => { 
+  let format = "(xxx) xxx-xxxx"
+
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace("x", numbers[i])
+  }
+
+  return format
+  }
